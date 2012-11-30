31eb7d9f8ad4b7ffb48785ba892a52988b43ae3e
Frederik Granna <frederik@tape.io>
2012-11-30 22:33:06 +0100

    Renamed Sweep example

--- examples/Sweep/Sweep.ino
+++ examples/Sweep/Sweep.ino
@@ -1,6 +1,6 @@
 #include "GroveLedbar.h"
 
-
+// Pin definition
 int dataPin = 12;
 int clockPin = 13;
 
@@ -14,9 +14,9 @@ void loop() {
   unsigned int i=0x0000;
   while(i<=0x03ff) {
     strip.setCmdMode();
-    strip.sendLED(i);
+    strip.sendLED(i); // Set LED States
     strip.latchData();
     i=i*2+1;
-   delay(10); 
+   delay(100); 
   }
 }
